30326b00cbc8502b052cee9255328e4e299842ac
patrickvl <patrickvl@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2008-05-07 22:44:07 +0000

    Hmm, renaming of a few files has gone wrong - for now I just added the newly named files (next time I'll use SVN rename). Also my previous commit lacked a description (blame that on my first time use of Subversion), so here we go:
    I've tried to straighten the code to the Delphi standard (begin/end on separate lines, 2 space indents, etc) and for the most occurring symbols I assured same-case-spelling (the keyword 'string' in lowercase, all others PascalCased).
    
    Also I started translating a bit of XIso's code, about 3% done (it's quite a big project).

---
+++ src/Tools/XIso/src/GenerateXDFS.pas
@@ -0,0 +1,56 @@
+{
+   xISO
+   Copyright 1984, 1986, 1989, 1992, 2000, 2001, 2002
+   Free Software Foundation, Inc.
+
+   This file is part of xISO, made it by Yursoft.com
+
+   xISO is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   Bison is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Bison; see the file COPYING.  If not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+}
+unit GenerateXDFS;
+
+interface
+
+uses
+  // Delphi
+  Classes, Windows;
+
+type
+  TGenerateXDFS = class(TThread)
+  protected
+    procedure Execute; override;
+  end;
+
+var
+  Imagen, Folder: string;
+
+implementation
+
+uses
+  // XIso
+  uxisomaker, TextConsts;
+
+{ GenerateXDFS }
+
+procedure TGenerateXDFS.Execute;
+begin
+  if not CrearXISO(Imagen, Folder) then
+    MessageBox(Handle, PChar(SErrorCrearXISO), PChar(SMessage), MB_OK or MB_ICONERROR)
+  else
+    MessageBox(Handle, PChar(SImagenCreadaOK), PChar(SMessage), MB_OK or MB_ICONINFORMATION);
+end;
+
+end.
