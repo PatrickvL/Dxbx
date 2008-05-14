f6fa050cd139cb3a9630e1e2dfdcf7b53d40eb45
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2008-05-14 19:00:57 +0000

    made some want changes

--- build.cmd
+++ build.cmd
@@ -7,11 +7,20 @@ echo ** DXBX Build process.                       **
 echo **                                           **
 echo ***********************************************
 
+
+echo Create Bin dir.
+md Bin
+echo Create Dcu dir.
+md Dcu
+
 echo Build Dxbx
 want\want.exe -buildfile want\dxbx.xml 
 
 echo Build CxbxKrnl.dll
 want\want.exe -buildfile want\Cxbxkrnl.xml 
+
+echo create Bin\Tools
+md Bin\Tools
 
 echo Build XdkTracker
 want\want.exe -buildfile want\XdkTracker.xml 
