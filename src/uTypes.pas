77ed0cb8460d62295a412ea9ce1d751b2dd55553
shadowtj <shadowtj@shadowtj.org>
2008-06-04 18:07:00 +0000

    heavy code cleanup and changing tmainmenu names to the code style

--- src/uTypes.pas
+++ src/uTypes.pas
@@ -29,6 +29,8 @@ const
   NUMBER_OF_THUNKS = 367;
 
 type
+  EMU_STATE = ( esNone, esFileOpen, esRunning );
+
   TDebugInfoType = (ditConsole, ditFile);
   EnumAutoConvert = (CONVERT_TO_MANUAL, CONVERT_TO_XBEPATH, CONVERT_TO_WINDOWSTEMP);
   DebugMode = (DM_NONE, DM_CONSOLE, DM_FILE);
