69544add1402b415b64b1adbc071d20c33dd0ec1
patrickvl <patrickvl@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2008-06-16 06:04:52 +0000

    Missing file (thanks RoadTrain!)

---
+++ src/DxbxKrnl/uHLEDatabase.pas
@@ -0,0 +1,53 @@
+(*
+    This file is part of Dxbx - a XBox emulator written in Delphi (ported over from cxbx)
+    Copyright (C) 2007 Shadow_tj and other members of the development team.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+unit uHLEDatabase;
+
+{$INCLUDE ..\Dxbx.inc}
+
+interface
+
+uses
+  // Jedi
+  JwaWinType;
+  
+var
+  szHLELastCompileTime: PChar; // extern
+  HLEDataBaseSize: uint32; // extern
+  UnResolvedXRefs: uint32; // extern
+  bXRefFirstPass: bool; // extern
+  
+type
+// ******************************************************************
+// * HLEDataBase
+// ******************************************************************
+  HLEData = record // extern
+    _Library: PChar;
+
+    MajorVersionL: uint16;
+    MinorVersion: uint16;
+    BuildVersion: uint16;
+
+//    OovpaTable: POOVPATable;
+    OovpaTableSize: uint32;
+  end;
+//HLEDataBase[];
+
+implementation
+
+end.
+
