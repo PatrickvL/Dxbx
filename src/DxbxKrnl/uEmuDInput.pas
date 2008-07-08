3644996a12138f803e49bd001cf32c7433f656d4
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2008-07-08 11:07:40 +0000

    removed errors

--- src/DxbxKrnl/uEmuDInput.pas
+++ src/DxbxKrnl/uEmuDInput.pas
@@ -62,10 +62,11 @@ end;
 // func: XTL::EmuPollController
 procedure XTL_EmuDInputPoll(Controller: XINPUT_STATE);
 begin
-  g_XBController.ListenPoll(Controller);
+  (*g_XBController.ListenPoll(Controller);
+  *)
 
   if (Error_GetError <> '' ) then
-    MessageDlg( 'Dxbx[* UNHANDLED! *]', mtError, mbOK, 0 ); // TODO: Handle this! *)
+    ShowMessage( 'Dxbx[* UNHANDLED! *]'); // TODO: Handle this! *)
 end;
 
 end.
