7af08eb6becaa3ad9d35539e5692aa70c078038b
shadowtj <shadowtj@shadowtj.org>
2009-12-18 09:24:08 +0000

    small Delphi 2009/2010 warning fix CharInSet is added for Delphi 2007 compiler in the DxbxUtils.pas
    Delphi 2009 and 2010 gives warnings when using 
    char in ['a'..'z']
    
    this needs to be translated to 
    CharInSet(char, ['a'..'z']) 
    
    the CharInSet is not supported in standard Delphi 2007,
    so this function has been added in the Dxbxutils.pas with a compiler version check.  {$if compilerversion <= 18.5}

--- Projects/d12/TranslationChecker.dpr
+++ Projects/d12/TranslationChecker.dpr
@@ -2,7 +2,9 @@ program TranslationChecker;
 
 uses
   Forms,
-  uTranslationChecker in '..\..\src\Tools\TranslationChecker\uTranslationChecker.pas' {Form1};
+  uTranslationChecker in '..\..\src\Tools\TranslationChecker\uTranslationChecker.pas' {Form1},
+  uDxbxUtils in '..\..\src\uDxbxUtils.pas',
+  uTypes in '..\..\src\uTypes.pas';
 
 {$R *.res}
 
