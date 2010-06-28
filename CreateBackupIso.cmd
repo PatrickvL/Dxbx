806ed1423bed3d77a621d4a4e7e5cb3e8d8dc0b3
shadowtj <shadowtj@shadowtj.org>
2010-06-28 07:40:37 +0000

    Delete old tools and scripts that we don`t use

--- CreateBackupIso.cmd
+++ CreateBackupIso.cmd
@@ -15,25 +15,23 @@ REM Remove Bin Directory
 want\deltree /fBackup /s
 MD Backup
 MD Backup\Docs 
-MD Backup\DUnit 
 MD Backup\Libraries 
+MD Backup\Projects
 MD Backup\resources 
 MD Backup\Setup 
 MD Backup\src 
 MD Backup\Tools 
-MD Backup\Want 
+ 
 
 REM Copy backup files to Backup folder
 Copy *.* Backup
 XCopy Docs Backup\Docs /e
-XCopy DUnit Backup\DUnit /e
 XCopy Libraries Backup\Libraries /e
+XCopy Projects Backup\Projects /e
 XCopy resource Backup\resources /e
 XCopy Setup Backup\Setup /e
 XCopy src Backup\src /e
 XCopy Tools Backup\Tools /e
-XCopy Want Backup\Want /e
-
 
 
 echo Create Backup.iso
