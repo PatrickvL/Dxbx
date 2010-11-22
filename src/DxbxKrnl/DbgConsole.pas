e9e1abdf8c8815e6994e089baab07adfdf6875a4
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2010-11-22 11:21:58 +0000

    DbgConsole translations. now it also compiles again.

--- src/DxbxKrnl/DbgConsole.pas
+++ src/DxbxKrnl/DbgConsole.pas
@@ -5,8 +5,13 @@ interface
 uses
   // Delphi
   Windows,
+  SysUtils,
+  StrUtils,
   // Dxbx
   uLog,
+  uDxbxKrnlUtils,
+  uDxbxUtils,
+  uEmuAlloc,
   uResourceTracker;
 
 Type
@@ -43,6 +48,7 @@ implementation
 {$ifdef _DEBUG_TRACK_VB) or defined(_DEBUG_TRACK_PB)}
 
 procedure EnableTracker(trackTotal: ResourceTracker; tracker: ResourceTracker; a: integer; b:integer; action:ETAction);
+// Branch:shogun  Revision:0.8.1-Pre2  Translator:PatrickvL  Done:100
 var
   v: Integer;
   i: Integer;
@@ -101,238 +107,219 @@ end;
 { TDbConsole }
 
 procedure TDbConsole.DbgConsole;
+// Branch:shogun  Revision:0.8.1-Pre2  Translator:PatrickvL  Done:90
 begin
-  m_cur = 0;
+  m_cur := 0;
 
   DbgPrintf('DxbxDbg');
 //  fflush(stdout);
 
-  m_szInput[0] = '#0';
+  m_szInput[0] := #0;
 end;
 
 procedure TDbConsole.ParseCommand;
+// Branch:shogun  Revision:0.8.1-Pre2  Translator:PatrickvL  Done:80
+var
+  szCmd : Array [0..32-1] of AnsiChar;
+  Full, v, n, m, c: Integer;
+  cur: PRTNode;
+  enabled: Boolean;
 begin
-(*    printf("\n");
+  Dbgprintf('');
 
-    char szCmd[32];
 
-    szCmd[0] = '\0';
+  szCmd[0] := #0;
 
-    sscanf(m_szInput, "%s", szCmd);
+(*  sscanf(m_szInput, '%s', szCmd); *)
 
-    // TODO: as command list grows, turn into static string/ptr lookup
+  // TODO: as command list grows, turn into static string/ptr lookup
+  if (CompareStr(szCmd, 'h') = 0) or (CompareStr(szCmd, 'help') = 0) then
+  begin
+    Dbgprintf('DxbxDbg: ');
+    Dbgprintf('DxbxDbg: Cxbx Debug Command List:');
+    Dbgprintf('DxbxDbg: ');
+    Dbgprintf('DxbxDbg:  Help            [H]     : Show Command List');
+    Dbgprintf('DxbxDbg:  Quit/Exit       [Q]     : Stop Emulation');
+    Dbgprintf('DxbxDbg:  Trace           [T]     : Toggle Debug Trace');
 
-    if(_stricmp(szCmd, "h") == 0 || _stricmp(szCmd, "help") == 0)
-    {
-        printf("CxbxDbg: \n");
-        printf("CxbxDbg: Cxbx Debug Command List:\n");
-        printf("CxbxDbg: \n");
-        printf("CxbxDbg:  Help            [H]     : Show Command List\n");
-        printf("CxbxDbg:  Quit/Exit       [Q]     : Stop Emulation\n");
-        printf("CxbxDbg:  Trace           [T]     : Toggle Debug Trace\n");
+    {$ifdef _DEBUG_TRACK_VB}
+    Dbgprintf('DxbxDbg:  ListVB          [LVB]   : List Active Vertex Buffers');
+    Dbgprintf('DxbxDbg:  DisableVB       [DVB #] : Disable Active Vertex Buffer(s)');
+    Dbgprintf('DxbxDbg:  EnableVB        [EVB #] : Enable Active Vertex Buffer(s)');
+    Dbgprintf('DxbxDbg:  DumpStreamCache [DSC]   : Dumps the patched streams cache');
+    {$endif}
 
-        #ifdef _DEBUG_TRACK_VB
-        printf("CxbxDbg:  ListVB          [LVB]   : List Active Vertex Buffers\n");
-        printf("CxbxDbg:  DisableVB       [DVB #] : Disable Active Vertex Buffer(s)\n");
-        printf("CxbxDbg:  EnableVB        [EVB #] : Enable Active Vertex Buffer(s)\n");
-        printf("CxbxDbg:  DumpStreamCache [DSC]   : Dumps the patched streams cache\n");
-        #endif
+    {$ifdef _DEBUG_TRACK_PB}
+    Dbgprintf('DxbxDbg:  ListPB          [LPB]   : List Active Push Buffers');
+    Dbgprintf('DxbxDbg:  ShowPB          [SPB #] : Show Push Buffer(s)');
+    Dbgprintf('DxbxDbg:  DisablePB       [DPB #] : Disable Push Buffer(s)');
+    Dbgprintf('DxbxDbg:  EnablePB        [EPB #] : Enable Push Buffer(s)');
+    Dbgprintf('DxbxDbg:  ClearPB         [CPB]   : Clear Push Buffer List');
+    {$endif}
 
-        #ifdef _DEBUG_TRACK_PB
-        printf("CxbxDbg:  ListPB          [LPB]   : List Active Push Buffers\n");
-        printf("CxbxDbg:  ShowPB          [SPB #] : Show Push Buffer(s)\n");
-        printf("CxbxDbg:  DisablePB       [DPB #] : Disable Push Buffer(s)\n");
-        printf("CxbxDbg:  EnablePB        [EPB #] : Enable Push Buffer(s)\n");
-        printf("CxbxDbg:  ClearPB         [CPB]   : Clear Push Buffer List\n");
-        #endif
+    {$ifdef _DEBUG_ALLOC}
+    Dbgprintf('DxbxDbg:  DumpMem         [DMEM]  : Dump the heap allocation tracking table');
+    {$endif} // _DEBUG_ALLOCC
 
-        #ifdef _DEBUG_ALLOC
-        printf("CxbxDbg:  DumpMem         [DMEM]  : Dump the heap allocation tracking table\n");
-        #endif // _DEBUG_ALLOCC
+    Dbgprintf('DxbxDbg:  CLS');
+    Dbgprintf('DxbxDbg: ');
+    Dbgprintf('DxbxDbg: # denotes parameter of form [#] or [#-#]');
+    Dbgprintf('DxbxDbg: ');
+  end
+  else if (CompareStr(szCmd, 'q') = 0) or (CompareStr(szCmd, 'quit') = 0) or (CompareStr(szCmd, 'exit') = 0) then
+  begin
+    Dbgprintf('DxbxDbg: Goodbye...');
+    DxbxKrnlCleanup('');
+  end
+  else if (CompareStr(szCmd, 't') = 0) or (CompareStr(szCmd, 'trace') = 0) then
+  begin
+(*        g_bPrintfOn = not g_bPrintfOn;
+      Dbgprintf('DxbxDbg: Trace is now %s', ifthen(g_bPrintfOn, 'ON', 'OFF')); *)
+  end
+  {$ifdef _DEBUG_TRACK_VB}
+  else if(CompareStr(szCmd, 'lvb') = 0) or (CompareStr(szCmd, 'ListVB') = 0) then
+  begin
+    v := 0;
 
-        printf("CxbxDbg:  CLS\n");
-        printf("CxbxDbg: \n");
-        printf("CxbxDbg: # denotes parameter of form [#] or [#-#]\n");
-        printf("CxbxDbg: \n");
-    }
-    else if(_stricmp(szCmd, "q") == 0 || _stricmp(szCmd, "quit") == 0 || _stricmp(szCmd, "exit") == 0)
-    {
-        printf("CxbxDbg: Goodbye...\n");
-        CxbxKrnlCleanup(NULL);
-    }
-    else if(_stricmp(szCmd, "t") == 0 || _stricmp(szCmd, "trace") == 0)
-    {
-        g_bPrintfOn = !g_bPrintfOn;
-        printf("CxbxDbg: Trace is now %s\n", g_bPrintfOn ? "ON" : "OFF");
-    }
-    #ifdef _DEBUG_TRACK_VB
-    else if(_stricmp(szCmd, "lvb") == 0 || _stricmp(szCmd, "ListVB") == 0)
-    {
-        int v=0;
+    g_VBTrackTotal.Lock();
 
-        g_VBTrackTotal.Lock();
+    cur := g_VBTrackTotal.getHead();
 
-        RTNode *cur = g_VBTrackTotal.getHead();
+    while(cur <> nil) and (cur.pNext <> nil) do
+    begin
+      enabled := not g_VBTrackDisable.exists(cur.pResource);
 
-        while(cur != NULL && cur->pNext != NULL)
-        {
-            bool enabled = !g_VBTrackDisable.exists(cur->pResource);
+      Inc(v);
+      Dbgprintf('DxbxDbg: %.02d : 0x%.08X (%s)', [v, cur.pResource, ifthen(enabled, 'enabled', 'disabled')]);
 
-            printf("CxbxDbg: %.02d : 0x%.08X (%s)\n", v++, cur->pResource, enabled ? "enabled" : "disabled");
+      cur := cur.pNext;
+    end;
 
-            cur = cur->pNext;
-        }
+    g_VBTrackTotal.Unlock();
+  end
+  else if(CompareStr(szCmd, 'dvb') = 0) or (CompareStr(szCmd, 'DisableVB') = 0) then
+  begin
+    n := 0;
+    m := 0;
 
-        g_VBTrackTotal.Unlock();
-    }
-    else if(_stricmp(szCmd, "dvb") == 0 || _stricmp(szCmd, "DisableVB") == 0)
-    {
-        int n=0, m=0;
+    c := sscanf(m_szInput, '%*s %d-%d', @n, @m);
 
-        int c = sscanf(m_szInput, "%*s %d-%d", &n, &m);
-
-        if(c == 1)
-        {
-            EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, n, ETA_DISABLE);
-        }
-        else if(c == 2)
-        {
-            EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, m, ETA_DISABLE);
-        }
-        else
-        {
-            printf("CxbxDbg: Syntax Incorrect (dvb #)\n");
-        }
-    }
-    else if(_stricmp(szCmd, "evb") == 0 || _stricmp(szCmd, "EnableVB") == 0)
-    {
-        int n=0, m=0;
-
-        int c = sscanf(m_szInput, "%*s %d-%d", &n, &m);
-
-        if(c == 1)
-        {
-            EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, n, ETA_ENABLE);
-        }
-        else if(c == 2)
-        {
-            EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, m, ETA_ENABLE);
-        }
-        else
-        {
-            printf("CxbxDbg: Syntax Incorrect (dvb #)\n");
-        }
-    }
-    else if(_stricmp(szCmd, "dsc") == 0 || _stricmp(szCmd, "DumpStreamCache") == 0)
-    {
-        XTL::VertexPatcher::DumpCache();
-    }
-    #endif
-    #ifdef _DEBUG_TRACK_PB
-    else if(_stricmp(szCmd, "lpb") == 0 || _stricmp(szCmd, "ListPB") == 0)
-    {
-        int v=0;
-
-        g_PBTrackTotal.Lock();
-
-        RTNode *cur = g_PBTrackTotal.getHead();
-
-        while(cur != NULL && cur->pNext != NULL)
-        {
-            bool enabled = !g_PBTrackDisable.exists(cur->pResource);
-
-            printf("CxbxDbg: %.02d : 0x%.08X (%s)\n", v++, cur->pResource, enabled ? "enabled" : "disabled");
-
-            cur = cur->pNext;
-        }
-
-        g_PBTrackTotal.Unlock();
-    }
-    else if(_stricmp(szCmd, "spb") == 0 || _stricmp(szCmd, "ShowPB") == 0)
-    {
-        int n=0, m=0;
-
-        int c = sscanf(m_szInput, "%*s %d-%d", &n, &m);
-
-        if(c == 1)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackShowOnce, n, n, ETA_SHOW);
-        }
-        else if(c == 2)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackShowOnce, n, m, ETA_SHOW);
-        }
-        else
-        {
-            printf("CxbxDbg: Syntax Incorrect (spb #)\n");
-        }
-    }
-    else if(_stricmp(szCmd, "dpb") == 0 || _stricmp(szCmd, "DisablePB") == 0)
-    {
-        int n=0, m=0;
-
-        int c = sscanf(m_szInput, "%*s %d-%d", &n, &m);
-
-        if(c == 1)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, n, ETA_DISABLE);
-        }
-        else if(c == 2)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, m, ETA_DISABLE);
-        }
-        else
-        {
-            printf("CxbxDbg: Syntax Incorrect (dpb #)\n");
-        }
-    }
-    else if(_stricmp(szCmd, "epb") == 0 || _stricmp(szCmd, "EnablePB") == 0)
-    {
-        int n=0, m=0;
-
-        int c = sscanf(m_szInput, "%*s %d-%d", &n, &m);
-
-        if(c == 1)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, n, ETA_ENABLE);
-        }
-        else if(c == 2)
-        {
-            EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, m, ETA_ENABLE);
-        }
-        else
-        {
-            printf("CxbxDbg: Syntax Incorrect (dpb #)\n");
-        }
-    }
-    else if(_stricmp(szCmd, "cpb") == 0 || _stricmp(szCmd, "ClearPB") == 0)
-    {
-        g_PBTrackTotal.clear();
-
-        printf("CxbxDbg: Push Buffer List Cleared!\n");
-    }
-    #endif
-    #ifdef _DEBUG_ALLOC
-    else if(_stricmp(szCmd, "dmem") == 0 || _stricmp(szCmd, "DumpMem") == 0)
-    {
-        int Full;
-        int c = sscanf(m_szInput, "%*s %d", &Full);
-        CxbxAllocDump(Full != 0);
-    }
-    #endif // _DEBUG_ALLOC
-    else if(_stricmp(szCmd, "cls") == 0)
-    {
-        // clear screen using system call
-        system("cls");
-    }
+    case  c of
+      1 : EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, n, ETA_DISABLE);
+      2 : EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, m, ETA_DISABLE);
     else
-    {
-        printf("CxbxDbg: Cmd \"%s\" not recognized!\n", szCmd);
-    }
-*)
+      Dbgprintf('DxbxDbg: Syntax Incorrect (dvb #)');
+    end;
+  end
+  else if(CompareStr(szCmd, 'evb') = 0) or (CompareStr(szCmd, 'EnableVB') = 0) then
+  begin
+    n := 0;
+    m := 0;
+
+    c := sscanf(m_szInput, '%*s %d-%d', @n, @m);
+
+    case c of
+      1 : EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, n, ETA_ENABLE);
+      2 : EnableTracker(g_VBTrackTotal, g_VBTrackDisable, n, m, ETA_ENABLE);
+    else
+      Dbgprintf('DxbxDbg: Syntax Incorrect (dvb #)');
+    end;
+  end;
+  else if(CompareStr(szCmd, 'dsc') = 0) or (CompareStr(szCmd, 'DumpStreamCache') = 0) then
+  begin
+     (* XTL_VertexPatcher. DumpCache(); *)
+  end
+  {$endif}
+  {$ifdef _DEBUG_TRACK_PB}
+  else if(CompareStr(szCmd, 'lpb') = 0) or (CompareStr(szCmd, 'ListPB') = 0) then
+  begin
+    v := 0;
+
+    g_PBTrackTotal.Lock();
+
+    cur := g_PBTrackTotal.getHead();
+
+    while(cur <> nil) and (cur.pNext <> nil) then
+    begin
+      enabled := not g_PBTrackDisable.exists(cur.pResource);
+
+      Inc(v);
+      Dbgprintf('DxbxDbg: %.02d : 0x%.08X (%s)', [v, cur.pResource, ifthen(enabled, 'enabled', 'disabled']);
+
+      cur := cur.pNext;
+    end;
+
+    g_PBTrackTotal.Unlock();
+  end
+  else if(CompareStr(szCmd, 'spb') = 0) or (CompareStr(szCmd, 'ShowPB') = 0) then
+  begin
+    n := 0;
+    m := 0;
+
+    c := sscanf(m_szInput, '%*s %d-%d', @n, @m);
+
+    case c of
+      1 : EnableTracker(g_PBTrackTotal, g_PBTrackShowOnce, n, n, ETA_SHOW);
+      2 : EnableTracker(g_PBTrackTotal, g_PBTrackShowOnce, n, m, ETA_SHOW);
+    else
+      Dbgprintf('DxbxDbg: Syntax Incorrect (spb #)');
+    end;
+  end
+  else if(CompareStr(szCmd, 'dpb') = 0) or (CompareStr(szCmd, 'DisablePB') = 0) then
+  begin
+    n := 0;
+    m := 0;
+
+    c := sscanf(m_szInput, '%*s %d-%d', @n, @m);
+
+    case c of
+      1 : EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, n, ETA_DISABLE);
+      2 : EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, m, ETA_DISABLE);
+    else
+      Dbgprintf('DxbxDbg: Syntax Incorrect (dpb #)');
+    end;
+  end
+  else if(CompareStr(szCmd, 'epb') = 0) or (CompareStr(szCmd, 'EnablePB') = 0) then
+  begin
+    n := 0;
+    m := 0;
+
+    c := sscanf(m_szInput, '%*s %d-%d', @n, @m);
+
+    case c of
+      1 : EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, n, ETA_ENABLE);
+      2 : EnableTracker(g_PBTrackTotal, g_PBTrackDisable, n, m, ETA_ENABLE);
+    else
+      Dbgprintf('DxbxDbg: Syntax Incorrect (dpb #)');
+    end;
+  end
+  else if(CompareStr(szCmd, 'cpb') = 0) or (CompareStr(szCmd, 'ClearPB') = 0) then
+  begin
+    g_PBTrackTotal.clear();
+
+    Dbgprintf('DxbxDbg: Push Buffer List Cleared!');
+  end
+  {$endif}
+  {$ifdef _DEBUG_ALLOC}
+  else if(CompareStr(szCmd, 'dmem') = 0) or (CompareStr(szCmd, 'DumpMem') = 0) then
+  begin
+    c := sscanf(m_szInput, '%*s %d', @Full);
+    DxbxAllocDump(Full <> 0);
+  end
+  {$endif} // _DEBUG_ALLOC
+  else if(CompareStr(szCmd, 'cls') = 0) then
+  begin
+      // clear screen using system call
+(*        system('cls'); *)
+  end
+  else
+  begin
+      Dbgprintf('DxbxDbg: Cmd \%s\ not recognized!', [szCmd]);
+  end;
 end;
 
 procedure TDbConsole.Process;
+// Branch:shogun  Revision:0.8.1-Pre2  Translator:PatrickvL  Done:0
 begin
    // process all queued key presses
 (*    while(_kbhit())
@@ -371,21 +358,18 @@ begin
 end;
 
 procedure TDbConsole.Reset;
+// Branch:shogun  Revision:0.8.1-Pre2  Translator:PatrickvL  Done:80
 begin
-(*  m_cur = 0;
+  m_cur := 0;
 
-  fflush(stdout);
+//  fflush(stdout);
 
-  printf("\n");
+  Dbgprintf('');
 
-  printf("CxbxDbg> ");
-  fflush(stdout);
+  Dbgprintf('DxbxDbg> ');
+//  fflush(stdout);
 
-  m_szInput[0] = '\0'; *)
+  m_szInput[0] = #0;
 end;
-
-{$ifdef _DEBUG_TRACK_VB) or defined(_DEBUG_TRACK_PB)}
-
-{$endif}
 
 end.
