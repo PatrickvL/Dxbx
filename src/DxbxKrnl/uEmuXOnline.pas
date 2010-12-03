00c3eb457fa8af8a78c1c39f7a81141e4c031564
shadowtj <shadowtj@shadowtj.org>
2010-12-03 15:53:18 +0000

    Found a lot of XNet functions that have no patches for online functionality.
    XOnline functions need to be checked.. 
    these will follow in a next run.
    
    
    like functions:
    XOnlineLogon
    XOnlineFriendsGameInvite
    XOnlineGetNotification
    
    and a lot more.

--- src/DxbxKrnl/uEmuXOnline.pas
+++ src/DxbxKrnl/uEmuXOnline.pas
@@ -30,6 +30,7 @@ uses
   // Jedi Win32API
   JwaWinType,
   // Dxbx
+  uEmuKrnl,
   uTypes,
   uLog,
   uEmuFS;
@@ -67,6 +68,298 @@ begin
 
   Result := ret;
 end;
+
+function XTL_EmuXNetCleanup(): Integer; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetStartup').
+    LogEnd();
+
+  Unimplemented('EmuXNetCleanup');
+  Result := 0;
+
+  EmuSwapFS(fsXbox);
+end;
+
+function XTL_EmuXNetConnect
+(
+    ina: IN_ADDR
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    // Todo: Log ina
+    LogBegin('EmuXNetStartup').
+    //      _(ina, 'ina').
+    LogEnd();
+
+  Unimplemented('EmuXNetConnect');
+  Result := 0;
+
+  EmuSwapFS(fsXbox);
+end;
+
+// Todo: PXNKID and PXNKEY not availible
+(*function XTL_EmuXNetCreateKey
+(
+    pxnkid: PXNKID;
+    pxnKey: PXNKEY;
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetCreateKey').
+      _(pxnkid, 'pxnkid').
+      _(pxnKey, 'pxnKey').
+    LogEnd();
+
+  Unimplemented('EmuXNetCreateKey');
+  Result := 0;
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+// Todo: pszHost, hEvent, ppxndns not availible
+(*function XTL_EmuXNetDnsLookup
+(
+    pszHost: PChar;
+    hEvent: WSAEVENT;
+    ppxndns: PPXNDNS;
+): INT: stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetDnsLookup').
+      _(pszHost, 'pszHost').
+      _(hEvent, 'hEvent').
+      _(ppxndns, 'ppxndns').
+    LogEnd();
+
+  Unimplemented('EmuXNetDnsLookup');
+  Result := 0;
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+// Todo: PXNDNS not availible
+(*function XTL_EmuXNetDnsRelease
+(
+    pxndns: PXNDNS
+): INT: stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetDnsRelease').
+      _(pxndns, 'pxndns').
+    LogEnd();
+
+  Unimplemented('EmuXNetDnsRelease');
+  Result := 0;
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+function XTL_EmuXNetGetConnectStatus
+(
+    ina: IN_ADDR
+): DWORD; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    // Todo: Log ina
+    LogBegin('XNetGetConnectStatus').
+//      _(ina, 'ina').
+    LogEnd();
+
+  Unimplemented('XNetGetConnectStatus');
+
+  EmuSwapFS(fsXbox);
+end;
+
+// TODO: PXNADDR not availible
+(*function XTL_EmuXNetGetDebugXnAddr
+(
+    pxna: PXNADDR
+): DWORD; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetGetDebugXnAddr').
+      _(pxna, 'pxna').
+    LogEnd();
+
+  Unimplemented('EmuXNetGetDebugXnAddr');
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+// TODO: PXNADDR not availible
+(*function XTL_EmuXNetGetTitleXnAddr
+(
+    pxna: PXNADDR
+): DWORD; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetGetTitleXnAddr').
+      _(pxna, 'pxna').
+    LogEnd();
+
+  Unimplemented('EmuXNetGetTitleXnAddr');
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+procedure XTL_EmuXNetInAddrToString
+(
+    ina: IN_ADDR;
+    pchBuf: Pchar;
+    cchBuf: INT
+); stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    // Todo: Log ina
+    LogBegin('EmuXNetInAddrToString').
+//      _(ina, 'ina').
+      _(pchBuf, 'pchBuf').
+      _(cchBuf, 'cchBuf').
+    LogEnd();
+
+  Unimplemented('EmuXNetInAddrToString');
+
+  EmuSwapFS(fsXbox);
+end;
+
+// Todo: PXNADDR and PXNKID not availible
+(*function XTL_EmuXNetInAddrToXnAddr
+(
+    ina: IN_ADDR;
+    pxna: PXNADDR;
+    pxnkid: PXNKID
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    LogBegin('EmuXNetInAddrToXnAddr').
+      _(ina, 'ina').
+      _(pxna, 'pxna').
+      _(pxnkid, 'pxnkid').
+    LogEnd();
+
+  Unimplemented('EmuXNetInAddrToXnAddr');
+
+  EmuSwapFS(fsXbox);
+end; *)
+
+// TODO: Implement
+(*function XTL_EmuXNetQosListen(
+    XNKID * pxnkid,
+    BYTE * pb,
+    UINT cb,
+    DWORD dwBitsPerSec,
+    DWORD dwFlags
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+
+end; *)
+
+// Todo: Implement
+(*function XTL_EmuXNetQosLookup(
+    UINT cxna,
+    XNADDR * apxna[],
+    XNKID * apxnkid[],
+    XNKEY * apxnkey[],
+    UINT cina,
+    IN_ADDR aina[],
+    DWORD adwServiceId[],
+    UINT cProbes,
+    DWORD dwBitsPerSec,
+    DWORD dwFlags,
+    WSAEVENT hEvent,
+    XNQOS * * ppxnqos
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+egin
+
+end; *)
+
+// Todo: Implement
+(*function XTL_EmuXNetQosRelease
+(
+    XNQOS * pxnqos
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+
+end; *)
+
+// Todo: Implement
+(*function XTL_EmuXNetServerToInAddr
+(
+    ina: IN_ADDR;
+    dwServiceId: DWORD;
+    pina: PIN_ADDR
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+
+end; *)
+
+function XTL_EmuXNetUnregisterInAddr
+(
+    ina: IN_ADDR
+): INT; stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+  EmuSwapFS(fsWindows);
+
+  if MayLog(lfUnit) then
+    // Todo: Log ina
+    LogBegin('EmuXNetUnregisterInAddr').
+//      _(ina, 'ina').
+    LogEnd();
+
+  Unimplemented('EmuXNetUnregisterInAddr');
+
+  EmuSwapFS(fsXbox);
+
+  Result := 0;
+end;
+
+// Todo: Implement
+(*function XTL_EmuXNetXnAddrToInAddr
+(
+    const XNADDR *pxna,
+    const XNKID *pxnkid,
+    IN_ADDR *pina
+): INT: stdcall;
+// Branch:Dxbx  Translator:Shadow_Tj  Done:0
+begin
+
+end; *)
 
 function XTL_EmuXNetStartup
 (
@@ -243,11 +536,33 @@ end;
 
 
 
+
+
 exports
   XTL_EmuWSAStartup,
 
   XTL_EmuXNetGetEthernetLinkStatus,
   XTL_EmuXNetStartup, //: DXBX marked out for better logging
+
+  XTL_EmuXNetCleanup,
+  XTL_EmuXNetConnect,
+//  XTL_EmuXNetCreateKey, // not implemented
+//  XTL_EmuXNetDnsLookup, // not implemented
+//  XTL_EmuXNetDnsRelease, // not implemented
+  XTL_EmuXNetGetConnectStatus,
+//  XTL_EmuXNetGetDebugXnAddr, // not implemented
+  XTL_EmuXNetGetEthernetLinkStatus,
+//  XTL_EmuXNetGetTitleXnAddr, // not implemented
+  XTL_EmuXNetInAddrToString,
+//  XTL_EmuXNetInAddrToXnAddr, // not implemented
+//  XTL_EmuXNetQosListen, // not implemented
+//  XTL_EmuXNetQosLookup, // not implemented
+//  XTL_EmuXNetQosRelease, // not implemented
+//  XTL_EmuXNetServerToInAddr, // not implemented
+//  XTL_EmuXNetStartup, // not implemented
+  XTL_EmuXNetUnregisterInAddr,
+//  XTL_EmuXNetXnAddrToInAddr, // not implemented
+
 //  XTL_EmuXOnlineLaunchNewImage,
   XTL_EmuXOnlineLogon;
 
