a38393ad0f245b8cb8544539d16963a5930c9ccc
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2010-12-29 15:45:57 +0000

    Created build scripts however dxbxkrnl.dpr is not compiling correct yet.

---
+++ Build_Dxbx_All_D14 - Copy.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: Use local variables to prevent a mess in the environment variables
+setlocal
+
+echo --- Building Dxbx ---
+set DEFINES=""
+set SEARCH_PATH="..\..\src;..\..\Libraries\jwapi\Win32API;..\..\Libraries\Jcl\windows;..\..\Libraries\Jcl\common;..\..\Libraries\DirectX9"
+set INCLUDE_PATH="..\..\src\include;..\..\libraries\jcl\include;..\..\Libraries\jwapi\Includes"
+
+cd projects
+cd d14
+
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" PatternTrieBuilder.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" XBEExplorer.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" SvnBuilder.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" DxbxKrnl.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" Dxbx.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+
+cd..
+cd..
+
+Build_Setup.cmd
---
+++ Build_Dxbx_XBEExplorer_D14.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Use local variables to prevent a mess in the environment variables
+setlocal
+
+echo --- Building Dxbx ---
+set DEFINES=""
+set SEARCH_PATH="..\..\src;..\..\Libraries\jwapi\Win32API;..\..\Libraries\Jcl\windows;..\..\Libraries\Jcl\common;..\..\Libraries\DirectX9"
+set INCLUDE_PATH="..\..\src\include;..\..\libraries\jcl\include;..\..\Libraries\jwapi\Includes"
+
+cd projects
+cd d14
+
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" XBEExplorer.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+
+cd..
+cd..
+Build_Setup_XBEExplorer.cmd
