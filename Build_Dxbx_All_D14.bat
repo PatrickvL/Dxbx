a106165d18c8c4a527083c5a70c10d909ff4633d
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2011-02-08 10:54:27 +0000

    Dxbx kernel is now also working with build script. Defines for dll needed to be set.
    Also removed a hint in release build for dxbxkrnl.dll

--- Build_Dxbx_All_D14.bat
+++ Build_Dxbx_All_D14.bat
@@ -5,6 +5,7 @@ setlocal
 
 echo --- Building Dxbx ---
 set DEFINES=""
+set DEFINESKRNL="RELEASE;DXBX_DLL"
 set SEARCH_PATH="..\..\src;..\..\Libraries\jwapi\Win32API;..\..\Libraries\Jcl\windows;..\..\Libraries\Jcl\common;..\..\Libraries\DirectX9"
 set INCLUDE_PATH="..\..\src\include;..\..\libraries\jcl\include;..\..\Libraries\jwapi\Includes"
 
@@ -14,5 +15,5 @@ cd d14
 dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" PatternTrieBuilder.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
 dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" XBEExplorer.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
 dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" SvnBuilder.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
-dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" DxbxKrnl.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
+dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" DxbxKrnl.dpr -D%DEFINESKRNL% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
 dcc32 -$D+ -$L- -W -H- -B -Q -GD -E"..\..\bin" -N"DCU" -R"Resources" -U"Units" Dxbx.dpr -D%DEFINES% -U%SEARCH_PATH% -I%INCLUDE_PATH% -N0..\..\dcu
