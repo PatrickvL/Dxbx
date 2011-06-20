7b215242180b167b211e14e31f20d3415b9c8e12
shadowtj <shadowtj@268f5bea-7c72-4cc5-bae1-aebef56d899c>
2011-06-20 17:18:12 +0000

    TranslationChecker is compiling again

--- Projects/Delphi/D14/TranslationChecker.dpr
+++ Projects/Delphi/D14/TranslationChecker.dpr
@@ -2,8 +2,8 @@ program TranslationChecker;
 
 uses
   Forms,
-  uTypes in '..\..\src\uTypes.pas',
-  uTranslationChecker in '..\..\src\Tools\TranslationChecker\uTranslationChecker.pas' {Form1};
+  uTypes in '..\..\..\Source\Delphi\src\uTypes.pas',
+  uTranslationChecker in '..\..\..\Source\Delphi\src\Tools\TranslationChecker\uTranslationChecker.pas' {Form1};
 
 {$R *.res}
 
